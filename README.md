c3ab016824a316414a9a5331905e13464f5549e4
Wemerson Araujo <wemersontga@gmail.com>
2023-07-13 19:38:48 -0400

    Criação do projeto

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# project_gameList
- 
+# projeto lista de Games:
--- src/test/java/com/wemerson/dslist/DslistApplicationTests.java
+++ src/test/java/com/wemerson/dslist/DslistApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dslist;
+package com.wemerson.dslist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
