d16d8dd5bb2e42cc603050fbf811ef5bd3751b92
Wemerson Araujo <wemersontga@gmail.com>
2023-07-16 12:40:53 -0400

    Add novas camadas, Gamelist

---
+++ src/main/java/com/wemerson/dslist/dto/GameListDTO.java
@@ -0,0 +1,22 @@
+package com.wemerson.dslist.dto;
+
+import com.wemerson.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/com/wemerson/dslist/services/GameService.java
@@ -0,0 +1,39 @@
+package com.wemerson.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.wemerson.dslist.dto.GameDTO;
+import com.wemerson.dslist.dto.GameMinDTO;
+import com.wemerson.dslist.entities.Game;
+import com.wemerson.dslist.projections.GameMinProjection;
+import com.wemerson.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(GameMinDTO::new).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByGameList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(GameMinDTO::new).toList();
+	}
+}
